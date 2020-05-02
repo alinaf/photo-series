8e7725e0b0e5ff5e4ef63d3ad728f341549baad4
Alina Abidi <aabidi@usc.edu>
2020-05-02 00:42:12 -0700

    just simple fade to white

--- js/main.js
+++ js/main.js
@@ -14,46 +14,45 @@ $(window).load(function() {
 	$("body").removeClass("preload");
 });
 
-$(function () {
 
-    var $select = $('#select');
-    var $window = $(window);
-    var isFixed = false;
-    var init = $select.length ? $select.offset().top : 0;
+// Fade to white, but doesn't look right
+// $(function () {
+
+//     var $select = $('#select');
+//     var $window = $(window);
+//     var isFixed = false;
+//     var init = $select.length ? $select.offset().top : 0;
     
-    var $jumbo = $('#bg');
-    var jumboHeight = $jumbo.height();
+//     var $jumbo = $('#bg');
+//     var jumboHeight = $jumbo.height();
     
-    $window.scroll(function () {
-        var currentScrollTop = $window.scrollTop();
+//     $window.scroll(function () {
+//         var currentScrollTop = $window.scrollTop();
         
-        var currentJumboAlpha = Math.max(1 - ((currentScrollTop * 100 / jumboHeight) / 100), 0);
+//         var currentJumboAlpha = Math.max(1 - ((currentScrollTop * 100 / jumboHeight) / 100), 0);
     
-        $jumbo.css('opacity', currentJumboAlpha);
+//         $jumbo.css('opacity', currentJumboAlpha);
         
-        if (currentScrollTop > init && isFixed === false) {
-            isFixed = true;
-            $select.css({
-                top: 0,
-                position: 'fixed'
-            });
-            $('body').css('padding-top', $select.height());
-        } else if (currentScrollTop <= init && isFixed === true) {
-            isFixed = false;
-            $select.css('position', 'relative');
+//         if (currentScrollTop > init && isFixed === false) {
+//             isFixed = true;
+//             $select.css({
+//                 top: 0,
+//                 position: 'fixed'
+//             });
+//             $('body').css('padding-top', $select.height());
+//         } else if (currentScrollTop <= init && isFixed === true) {
+//             isFixed = false;
+//             $select.css('position', 'relative');
             
-            $('body').css('padding-top', 0);
-        }
-    });
+//             $('body').css('padding-top', 0);
+//         }
+//     });
 
-    $(".nav").click(function (e) {
-        e.preventDefault();
-        var divId = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(divId).offset().top - $select.height()
-        }, 500);
-    });
-
-
-
-});
+//     $(".nav").click(function (e) {
+//         e.preventDefault();
+//         var divId = $(this).attr('href');
+//         $('body').animate({
+//             scrollTop: $(divId).offset().top - $select.height()
+//         }, 500);
+//     });
+// });
