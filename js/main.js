b7dbbd17c8766a12489b99d39ba885b81018b7e8
Alina Abidi <aabidi@usc.edu>
2020-05-02 00:25:19 -0700

    lol @ attempt to fade to white

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 jQuery(window).scroll(function(){
+	console.log('hi');
     var full = 100; // distance to trigger
+    var fadeToWhite = 200;
     var scrolledFromtop = jQuery(window).scrollTop();
+    // console.log(scrolledFromtop);
 
     if(scrolledFromtop > full){
-        $("#bg").addClass('scrolled');
+    	// console.log(scrolledFromtop);
+    	$("#bg").addClass('scrolled');
+    // 	    if(scrolledFromtop > 300) {
+    // 			var nystories = document.querySelector("h1").offsetTop;
+    // 			var opac = (scrolledFromtop / nystories);
+    // 			console.log(opac);
+    // 			// alert(document.getElementById("bg").innerHTML);
+    // 			document.getElementById("bg").style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url('img/empty.png') no-repeat";
+    // 			// document.getElementById(#bg).style.background-image = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(img/empty.png) no-repeat";
+    // }
     }else{
-         $("#bg").removeClass('scrolled');
-    }
-});
+    	$("#bg").removeClass('scrolled');
+    	}
+    });
 
 $(window).load(function() {
-  $("body").removeClass("preload");
+	$("body").removeClass("preload");
 });
